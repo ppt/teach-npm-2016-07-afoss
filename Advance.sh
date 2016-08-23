79b44d4c2d3182046a5bd5ef57f90e205ffa255b
Praphan Theeravanvilai <praphan@angstromsolutions.com>
2016-08-23 12:01:27 +0700

    minor bug

--- Advance.sh
+++ Advance.sh
@@ -1,9 +1,9 @@
 # create OrderBook message
-getmsg md-message1-20160808.json OrderBook > data/OrderBook.json
+getmsg md-message1-20160808.log OrderBook > data/OrderBook.json
+grep -E 'ADVANC' data/OrderBook.json | relineno > data/ADVANC-Orderbook.json
 
-cat data/OrderBook.json | rmlineno | grep -E 'ADVANC' > ADVANC-Orderbook.json
-cat ADVANC-Orderbook.json | statfield displayName
+cat data/ADVANC-Orderbook.json | statfield displayName
 # only 9 fields
 
-cat data/ADVANC-Orderbook.json | node msgbody.js | node removekey.js timeStamp | node removekey.js closingPrice | sort | uniq | sort
+cat data/ADVANC-Orderbook.json | node msgbody.js | node removekey.js timeStamp | node removekey.js closingPrice | sort | uniq | sort > data/ADVANC-unique.json
 # 10 records include ODD Lots same displayName
